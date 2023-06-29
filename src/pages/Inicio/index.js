2d5cce1fde1b5741d0f1fb4d51a1046b522e990c
rafaelcorrea26 <rafael.fs.camaqua@gmail.com>
2023-06-29 09:46:44 -0300

    main

---
+++ src/pages/Inicio/index.js
@@ -0,0 +1,27 @@
+import Banner from "components/Banner";
+import Cabecalho from "components/Cabecalho";
+import Card from "components/Card";
+import Rodape from "components/Rodape";
+import Titulo from "components/Titulo";
+import videos from 'json/db.json';
+import styles from './Inicio.module.css';
+
+function Inicio() {
+    return (
+        <>
+            <Cabecalho />
+            <Banner imagem="home" />
+            <Titulo>
+                <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+            </Titulo>
+            <section className={styles.container}>
+                {videos.map((video) =>{
+                    return <Card {...video} key={video.id} />
+                })}
+            </section>
+            <Rodape />
+        </>
+    )
+}
+
+export default Inicio;
