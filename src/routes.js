189afdfaf7d003fdf41156e50c30b339ccb480d1
rafaelcorrea26 <rafael.fs.camaqua@gmail.com>
2023-08-05 13:56:15 -0300

    main

--- src/routes.js
+++ src/routes.js
@@ -5,22 +5,21 @@ import FavoritosProvider from "contextos/Favoritos";
 import Favoritos from "pages/Favoritos";
 import Inicio from "pages/Inicio";
 import Player from "pages/Player";
+import NaoEncontrada from "pages/NaoEncontrada";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PaginaBase from "pages/PaginaBase";
 
 function AppRoutes() {
     return (
         <BrowserRouter>
-            <Cabecalho />
-            <Container>
-                <FavoritosProvider>
-                    <Routes>
-                        <Route path="/" element={<Inicio />}></Route>
-                        <Route path="/favoritos" element={<Favoritos />}></Route>
-                        <Route path="/:id" element={<Player />}></Route>
-                    </Routes>
-                </FavoritosProvider>
-            </Container>
-            <Rodape />
+            <Routes>
+                <Route path="/" element={<PaginaBase />}>
+                    <Route index element={<Inicio />}></Route>
+                        <Route path="favoritos" element={<Favoritos />}></Route>
+                        <Route path=":id" element={<Player />}></Route>
+                        <Route path="*" element={<NaoEncontrada />}></Route>
+                </Route>
+            </Routes>
         </BrowserRouter>
     )
 }
